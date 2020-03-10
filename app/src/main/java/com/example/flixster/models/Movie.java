74a4a4d2c0295b0e2d55c0a5fabc961a171337c4
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2020-03-10 14:28:38 -0400

    first commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,43 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String overview;
+    String title;
+
+    public Movie(JSONObject jso) throws JSONException {
+        posterPath = jso.getString("poster_path");
+        overview = jso.getString("overview");
+        title = jso.getString("title");
+
+    }
+
+    public static List<Movie> fromJsonArray (JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
